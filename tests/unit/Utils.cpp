127327ffce9bfd7af6bd4e410cb5d4d0189072f5
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-06-28 00:24:52 -0300

    tests: test tries to replace a word that is not present in the string

--- tests/unit/Utils.cpp
+++ tests/unit/Utils.cpp
@@ -11,7 +11,18 @@ void test_should_replace_hello_world_by_ola_mundo()
     ASSERT_STREQ(expected, result);
 }
 
+void test_should_not_do_nothing_when_search_word_is_present_in_the_string()
+{
+    std::string input = "This phrase doesn't contain any word to replace";
+    std::string expected = "This phrase doesn't contain any word to replace";
+
+    std::string result = Utils::replace("hello", "hey", input);
+
+    ASSERT_STREQ(expected, result);
+}
+
 void RUN_UTILS_SUITE()
 {
     test_should_replace_hello_world_by_ola_mundo();
+    test_should_not_do_nothing_when_search_word_is_present_in_the_string();
 }
