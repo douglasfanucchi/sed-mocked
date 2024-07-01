f0a85a629188ae8bfac9b412080ee3f8c992d470
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-01 07:33:53 -0300

    tests: test creates replace file

--- tests/unit/Utils.cpp
+++ tests/unit/Utils.cpp
@@ -1,5 +1,8 @@
 #include <Utils.hpp>
 #include <asserts.hpp>
+#include <fstream>
+#include <iterator>
+#include <algorithm>
 
 void test_should_replace_hello_world_by_ola_mundo()
 {
@@ -31,9 +34,29 @@ void test_should_replace_multiple_recurrences()
     ASSERT_STREQ(expected, result);
 }
 
+void test_should_copy_a_file_and_its_name_should_be_its_original_name_dot_replace()
+{
+    std::fstream file("tests/files/lorem-ipsum.txt", std::ios::in);
+    std::string expected;
+    int expectedLines = std::count(std::istream_iterator<char>(file >> std::noskipws), {}, '\n');
+
+    Utils::create_replace_file("tests/files/lorem-ipsum.txt");
+
+    std::fstream resultFile("tests/files/lorem-ipsum.txt.replace");
+    int resultLines = std::count(std::istream_iterator<char>(resultFile >> std::noskipws), {}, '\n');
+    while (std::getline(file, expected)) {
+        std::string result;
+        std::getline(resultFile, result);
+
+        ASSERT_STREQ(expected, result);
+    }
+    ASSERT_EQ(expectedLines, resultLines);
+}
+
 void RUN_UTILS_SUITE()
 {
     test_should_replace_hello_world_by_ola_mundo();
     test_should_not_do_nothing_when_search_word_is_present_in_the_string();
     test_should_replace_multiple_recurrences();
+    test_should_copy_a_file_and_its_name_should_be_its_original_name_dot_replace();
 }
