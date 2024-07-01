01b313e2a9e5cdc8afed3f3128a18c75a6ed65a7
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-01 16:21:13 -0300

    tests: test replace the word foo with bar into the file

---
+++ tests/e2e/Program.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function ASSERT_STREQ()
+{
+if [[ "$1" == "$2" ]];
+then
+    printf ✅
+else
+    printf ❌
+    echo -en "\nEXPECTED\n$1\n";
+    echo -en "_______________________________________________\n";
+    echo -en "RESULT\n$2";
+fi
+}
+
+executable="valgrind --leak-check=full -q $1"
+
+function test_should_replace_into_the_file_the_word_foo_with_bar()
+{
+    local file="test.txt"
+    echo -n "foo" > $file
+    local args="$file foo bar"
+    local expected="bar"
+
+    eval "$executable $args"
+    local result=$(<$file)
+
+    ASSERT_STREQ "$expected" "$result"
+
+    rm -rf $file "$file.replace"
+}
+
+test_should_replace_into_the_file_the_word_foo_with_bar
+printf \\n
