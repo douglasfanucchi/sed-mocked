6ca22b725b031445e4a8d3b22715f94ec55666d5
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-01 22:54:52 -0300

    tests: test replaces multiple recurrences

--- tests/e2e/Program.sh
+++ tests/e2e/Program.sh
@@ -30,5 +30,21 @@ function test_should_replace_into_the_file_the_word_foo_with_bar()
     rm -rf $file "$file.replace"
 }
 
+function test_should_replace_into_the_file_the_sentence_hello_world_with_ola_mundo()
+{
+    local file="test.txt"
+    local args="$file 'hello world' 'ola mundo'"
+    echo -n "Writing this sentence hello world to be replaced, hello world" > $file
+    local expected="Writing this sentence ola mundo to be replaced, ola mundo"
+
+    eval "$executable $args"
+    local result=$(<$file)
+
+    ASSERT_STREQ "$expected" "$result"
+
+    rm -rf $file "$file.replace"
+}
+
 test_should_replace_into_the_file_the_word_foo_with_bar
+test_should_replace_into_the_file_the_sentence_hello_world_with_ola_mundo
 printf \\n
